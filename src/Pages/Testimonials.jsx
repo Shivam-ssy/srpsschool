b598f145b4dd62ac89f94580cf150310cb5a0bee
shivam Singh Yadav <shivamsinghyadavssy888@gmail.com>
2024-10-08 22:59:05 +0530

    responsive testimonials done

--- src/Pages/Testimonials.jsx
+++ src/Pages/Testimonials.jsx
@@ -6,10 +6,10 @@ function Testimonials() {
     <main className="w-full">
 
     <Header title="Testimonials" page="Testimonials"/>
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col mt-10 items-center w-full">
       <h3 className="font-bold text-3xl">Parent Says</h3>
       <span className="max-w-xl text-center my-5">Lorem ipsum  maxime inventore obcaecati placeat, voluptas minus quisquam numquam autem n</span>
-      <div className="flex flex-wrap max-w-5xl gap-5">
+      <div className="flex flex-wrap justify-center max-w-5xl gap-5">
         <TestimonialCard name="Narma J. Johnston" parentImg="../p2.jpg"/>
         <TestimonialCard name="Narma J. Johnston" parentImg="../p2.jpg"/>
         <TestimonialCard name="Narma J. Johnston" parentImg="../p2.jpg"/>
